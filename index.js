560f823de04ae7ad1bfd510f4b7b58e1184ba506
mdalamin <alaminbinesar@gmail.com>
2025-07-15 17:52:12 +0600

    add food api

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ dotenv.config();
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./admin-key.json");
+var serviceAccount = require("./Admin-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-
-
-// const serviceAccount = require("./admin-key.json");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j98yejq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // MongoDB Connection
-const client = new MongoClient(process.env.MONGODB_URI, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -48,6 +48,9 @@ const verifyFirebaseToken = async (req, res, next) => {
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
+    
+  
+    
     req.firebaseUser = decodedToken; // You can access user info like uid, email, etc.
     next();
   } catch (error) {
@@ -61,32 +64,31 @@ async function run() {
   try {
     await client.connect();
     const db = client.db("db_name");
-    const events = db.collection("collection");
+    const foodsCollection = db.collection("foods");
+    console.log("Connected to MongoDB");
+
+    app.post("/add-food", async (req, res) => {
+      const food = req.body;
+      const result = await foodsCollection.insertOne(food);
+      res.send(result);
+    });
+
+
+
   } finally {
   }
 }
 
 run().catch(console.dir);
 
-// const m1 = (req, res, next) =>{
-//   console.log("first")
-//   console.log("name ", req.name);
-//   req.name = "AR Arzu";
 
-//   next();
-// }
-
-// const m2 = (req, res, next) =>{
-//   console.log("name from m2 ", req.name);
-//   console.log("m2")
-// next();
-// }
 
 // Root route
 
 
 app.get("/", verifyFirebaseToken, async (req, res) => {
 console.log(req.firebaseUser);
+
 
   res.send("Server is running!");
 });
